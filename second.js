3e37520f5d8b51d49d5ea7c215bbf586cab9193e
vinodhleo <vinodhleo@outlook.com>
2022-03-30 16:52:53 +0530

    unsubscribe

--- second.js
+++ second.js
@@ -1,15 +1,29 @@
-import {Observable} from 'rxjs'
+import { interval, Observable} from 'rxjs'
+
+var count=0;
 
 
 function continousProvider(){
     return new Observable(subscriber=>{
-        setInterval(()=>{
+    setInterval(()=>{
+           
             subscriber.next(Math.round(Math.random()*100))
+            count++;
         },2000)
     })
 }
 
-continousProvider().subscribe({
+var subscription = continousProvider().subscribe({
     next: (x)=> console.log("received ",x)
 }
 )
+
+const temp=setInterval(()=>{
+    if(count==10){
+       subscription.unsubscribe();
+       console.log("Unsubscribed")
+       clearInterval(temp)
+    }
+    else
+       console.log("Checked still not unsubscribed")
+},1000)
